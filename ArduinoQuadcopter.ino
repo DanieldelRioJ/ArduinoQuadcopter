778acb7e3090dd3177113a125f75d4c47010d76f
Daniel del Río <daniel.delrio.juncal@gmail.com>
2024-02-09 19:22:42 +0100

    Yaw correction

--- ArduinoQuadcopter.ino
+++ ArduinoQuadcopter.ino
@@ -25,11 +25,15 @@ uint8_t fifoBuffer[64];
 Quaternion q;
 VectorFloat gravity;
 float ypr[3];
+float lastYaw = 0;
+float yawSpeed = 0;
 
 double Kp=0.5, Ki=0.005, Kd=0.05;
-double pitchCorrection = 0, rollCorrection = 0;
+double KpYaw=2, KiYaw=0, KdYaw=0;
+double pitchCorrection = 0, rollCorrection = 0, yawCorrection = 0;
 PID pidPitch((double*)&ypr[1], &pitchCorrection, &(rcCom.pitch), Kp, Ki, Kd, P_ON_E, DIRECT);
 PID pidRoll((double*)&ypr[2], &rollCorrection, &(rcCom.roll), Kp, Ki, Kd, P_ON_E, DIRECT);
+PID pidYaw((double*)&yawSpeed, &yawCorrection, &(rcCom.yaw), KpYaw, KiYaw, KdYaw, P_ON_E, DIRECT);
 
 void setup() {
 
@@ -99,6 +103,8 @@ void loop() {
             ypr[0] = ypr[0]*180/PI;
             ypr[1] = ypr[1]*180/PI;
             ypr[2] = ypr[2]*180/PI;
+            yawSpeed = ypr[0] - lastYaw;
+            lastYaw = ypr[0];
 
             /*Serial.print("ypr\t");
             Serial.print(ypr[0]);
@@ -106,6 +112,8 @@ void loop() {
             Serial.print(ypr[1]);
             Serial.print("\t");
             Serial.print(ypr[2]);
+            Serial.print("\t");
+            Serial.print(yawSpeed);
             Serial.println();*/
             
         }   
@@ -124,6 +132,8 @@ void _loop(){
         Serial.print(pitchCorrection);
         Serial.print("\trollCorrection = ");
         Serial.print(rollCorrection);
+        Serial.print("\tYawCorrection = ");
+        Serial.print(yawCorrection);
         Serial.print("\trollCorrection = ");
         Serial.println(rcCom.roll);
         if(rcCom.intD == 1000){
@@ -132,10 +142,10 @@ void _loop(){
             engineRR.setPower(rcCom.throttle);
             engineRL.setPower(rcCom.throttle);
         }else {            
-            engineFR.setPower(rcCom.throttle + rollCorrection - pitchCorrection);
-            engineFL.setPower(rcCom.throttle - rollCorrection - pitchCorrection);
-            engineRR.setPower(rcCom.throttle + rollCorrection + pitchCorrection);
-            engineRL.setPower(rcCom.throttle - rollCorrection + pitchCorrection);
+            engineFR.setPower(rcCom.throttle + rollCorrection - pitchCorrection + yawCorrection);
+            engineFL.setPower(rcCom.throttle - rollCorrection - pitchCorrection - yawCorrection);
+            engineRR.setPower(rcCom.throttle + rollCorrection + pitchCorrection - yawCorrection);
+            engineRL.setPower(rcCom.throttle - rollCorrection + pitchCorrection + yawCorrection);
         }
     }
 }
@@ -148,10 +158,15 @@ void _pidSetup(){
 
     pidRoll.SetMode(AUTOMATIC);
     pidRoll.SetSampleTime(10);
-    pidRoll.SetOutputLimits(-5,5); //-% and +% motor speed correction effect
+    pidRoll.SetOutputLimits(-5,5); //-% and +% motor speed correction effect    
+
+    pidYaw.SetMode(AUTOMATIC);
+    pidYaw.SetSampleTime(10);
+    pidYaw.SetOutputLimits(-5,5); //-% and +% motor speed correction effect
 }
 
 void _pidLoop(){
     pidPitch.Compute();
     pidRoll.Compute();
+    pidYaw.Compute();
 }
--- RCCom.cpp
+++ RCCom.cpp
@@ -1,6 +1,7 @@
 #include "RCCom.h"
 
 #define MAX_ANGLE 15
+#define MAX_ANGLE_SPEED 2 //PER READ
 
 void RCCom::setup(HardwareSerial &serial){
     this->ibus.begin(serial, IBUSBM_NOTIMER);
@@ -11,7 +12,7 @@ void RCCom::loop(){
     this->roll = -2 * MAX_ANGLE * ((float)this->ibus.readChannel(0)-1500)/1000;
     this->pitch = 2 * MAX_ANGLE * ((float)this->ibus.readChannel(1)-1500)/1000;
     this->throttle = ((float)this->ibus.readChannel(2) - 1000) / 10;
-    this->yaw = ((float)this->ibus.readChannel(3) - 1000) / 10;
+    this->yaw = 2 * MAX_ANGLE_SPEED * ((float)this->ibus.readChannel(3) - 1500) / 1000;
     this->p1 = this->ibus.readChannel(4);
     this->p2 = this->ibus.readChannel(5);
     this->intA = this->ibus.readChannel(6);
